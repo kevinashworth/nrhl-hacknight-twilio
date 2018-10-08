5528377eff29c2b3ed40d4fc624c73ea79d74fac
Kevin Ashworth <kevinashworth@yahoo.com>
2018-10-08 10:07:25 -0700

    Get the App going.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './App.js';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
+
 console.log(`Greetings from ${module.id}!`);
